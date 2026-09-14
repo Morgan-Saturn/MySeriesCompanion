92f66a1e3aa8137c2f9f645a9d6a701dafe9e5fe
Morgan-Saturn <morgan.tunteva@proton.me>
2026-09-14 11:48:33 +0200

    trying to configure daisyui. Rearrange a little the folders.

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="png" href="/assets/favicon.png" />
+    <title>MySeriesCompanion</title>
+</head>
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="ajout_series.html">Ajouter une série</a></li>
+        </ul>
+    </nav>
+    <h1>Bienvenue ! Qu'allez-vous regarder aujourd'hui ? Vos séries</h1>
+</body>
+</html>
