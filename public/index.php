bdb27d04ff174a2598cdc8725a85cf1085c6843e
Morgan-Saturn <morgan.tunteva@proton.me>
2026-09-14 12:22:55 +0200

    add the skeleton (many things from the architecture might change)

--- public/index.php
+++ public/index.php
@@ -3,16 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="png" href="/assets/favicon.png" />
+    <link rel="icon" type="png" href="assets/favicon.png" />
     <title>MySeriesCompanion</title>
 </head>
 <body>
-
-    <nav>
-        <ul>
-            <li><a href="ajout_series.html">Ajouter une série</a></li>
-        </ul>
-    </nav>
     <h1>Bienvenue ! Qu'allez-vous regarder aujourd'hui ? Vos séries</h1>
+
+    <a href="ajout_series.html"><button>Ajouter une série</button></a>
+
+<!-- popup  -->
 </body>
 </html>
